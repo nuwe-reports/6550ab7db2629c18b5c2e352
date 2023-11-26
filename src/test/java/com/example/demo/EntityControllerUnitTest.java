b9821afc51dfde9b11ec847edb072344b5baf2b3
Tetyana Pelykh <92629694+TetyanaPelykh@users.noreply.github.com>
2023-11-26 21:10:21 +0100

    Revert "Cambios"

--- src/test/java/com/example/demo/EntityControllerUnitTest.java
+++ src/test/java/com/example/demo/EntityControllerUnitTest.java
@@ -1,27 +1,44 @@
 
 package com.example.demo;
 
-
-
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import java.time.LocalDateTime;
+import java.time.format.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
 import com.example.demo.controllers.*;
 import com.example.demo.repositories.*;
 import com.example.demo.entities.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
+
+/** TODO
+ * Implement all the unit test in its corresponding class.
+ * Make sure to be as exhaustive as possible. Coverage is checked ;)
+ */
 
 @WebMvcTest(DoctorController.class)
 class DoctorControllerUnitTest{
@@ -32,16 +49,13 @@ class DoctorControllerUnitTest{
     @Autowired 
     private MockMvc mockMvc;
 
-    @Test
-    void shouldReturnAllDoctors() throws Exception {
-        when(doctorRepository.findAll()).thenReturn(Collections.singletonList(new Doctor("Nombre", "Apellido", 30, "email@hospital.com")));
+    @Autowired
+    private ObjectMapper objectMapper;
 
-        mockMvc.perform(get("/api/doctors"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.length()").value(1))
-                .andExpect(jsonPath("$[0].name").value("Nombre"))
-                .andExpect(jsonPath("$[0].age").value(30));
+    @Test
+    void this_is_a_test(){
+        // DELETE ME
+        assertThat(true).isEqualTo(false);
     }
 }
 
@@ -55,16 +69,13 @@ class PatientControllerUnitTest{
     @Autowired 
     private MockMvc mockMvc;
 
-    @Test
-    void shouldReturnAllPatients() throws Exception {
-        when(patientRepository.findAll()).thenReturn(Collections.singletonList(new Patient("NombrePaciente", "ApellidoPaciente", 25, "emailPaciente@hospital.com")));
+    @Autowired
+    private ObjectMapper objectMapper;
 
-        mockMvc.perform(get("/api/patients"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.length()").value(1))
-                .andExpect(jsonPath("$[0].name").value("NombrePaciente"))
-                .andExpect(jsonPath("$[0].age").value(25));
+    @Test
+    void this_is_a_test(){
+        // DELETE ME
+        assertThat(true).isEqualTo(false);
     }
 
 }
@@ -78,15 +89,13 @@ class RoomControllerUnitTest{
     @Autowired 
     private MockMvc mockMvc;
 
-    @Test
-    void shouldReturnAllRooms() throws Exception {
-        when(roomRepository.findAll()).thenReturn(Collections.singletonList(new Room("NombreSala")));
+    @Autowired
+    private ObjectMapper objectMapper;
 
-        mockMvc.perform(get("/api/rooms"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.length()").value(1))
-                .andExpect(jsonPath("$[0].name").value("NombreSala"));
+    @Test
+    void this_is_a_test(){
+        // DELETE ME
+        assertThat(true).isEqualTo(false);
     }
 
 }
